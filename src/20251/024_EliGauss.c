05b79c2f8dfee3ddf8bd35156534c08b350f309d
Mario Alberto Ibarra Manzano <ibarram@ugto.mx>
2025-03-20 15:59:23 -0600

    Update

---
+++ src/20251/024_EliGauss.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define NV 100
+
+int main(int argc, char *argv[])
+{
+	int n, i, j, k;
+	float A[NV][NV], b[NV], fct, x[NV];
+	do{
+		printf("Ingrese el numero de variables: ");
+		scanf("%d", &n);
+	}while(n<1||n>NV);
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			printf("A[%d][%d] = ", i+1, j+1);
+			scanf("%f", &A[i][j]);
+		}
+		printf("b[%d] = ", i+1);
+		scanf("%f", &b[i]);
+	}
+	printf("---\n");
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+			printf("%.2f\t", A[i][j]);
+		printf("%.2f\n", b[i]);
+	}
+	for(i=0; i<n-1; i++)
+		for(j=i+1; j<n; j++)
+		{
+			fct = A[j][i]/A[i][i];
+			for(k=0; k<n; k++)
+				A[j][k] -= (fct*A[i][k]);
+			b[j] -= fct*b[i];
+		}
+
+	for(i=0; i<n; i++)
+		x[i] = 0;
+	for(i=n-1; i>-1; i--)
+	{
+		x[i]=b[i];
+		for(j=0; j<n; j++)
+			if(i!=j)
+				x[i]-=A[i][j]*x[j];
+		x[i]/=A[i][i];
+		printf("x[%d] = %f\n", i+1, x[i]);
+	}
+	return 0;
+} 
