81a6f456cfe32d2ed5ca71baf78b62f4d2c2c830
Mario Alberto Ibarra Manzano <ibarram@users.noreply.github.com>
2026-09-03 15:19:29 -0600

    Reapply "Update"
    This reverts commit 606eda889cbcc597b69fc468d37441b4a3a1ca94.

---
+++ src/20262/Ejemplo016.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 10
+
+int main(int argc, char *argv[])
+{
+	int i, X[N];
+	srand((unsigned)time(NULL));
+	for(i=0; i<N; i++)
+	{
+		X[i] = rand();
+		printf("X[%d] = %d\n", i+1, X[i]);
+	}
+	return 0;
+}
